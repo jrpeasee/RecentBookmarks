37e238ee2c21821820034d2b62329582dfbe73a2
James Pease <jamespease@jamess-mbp-2.pk5001z>
2015-09-16 22:12:47 -0600

    added date info 2each bookmark

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@
 
 		
 
-			 if(recentList.childNodes.length === 0){
+			 
 
 			chrome.bookmarks.getRecent( 10, function(tree){
 
@@ -20,18 +20,19 @@
 				this.children.forEach(function(recent){
 					console.log(recent);
 					var u = recent.url;
-					var favicon = 'chrome://favicon/' + u;
+					var date = new Date(recent.dateAdded);
+					var dateTrim = date.toString().slice(0,15);
+					var favicon = 'https://www.google.com/s2/favicons?domain=' + u;
 					var recentUrl = '<img src="' + favicon + '"' + '>' + recent.url;
 					var li = document.createElement('li');
+					var date = document.createElement('span');
 					li.innerHTML = recentUrl;
+					li.appendChild(date);
+					date.innerHTML = dateTrim;
 					recentList.appendChild(li);
 				});
 				
 			});
-
-		   } else if(recentList.childNodes.length > 0){
-		   		$('#recentlist').empty();
-		   }
 		},
 
 		
