293a71a8c6caeecd392ad9f84c87dfe5f3436b56
James Pease <jamespease@Jamess-MacBook-Pro-2.local>
2018-03-19 19:00:37 -0700

    add padding to recentBookmarks id

--- js/main.js
+++ js/main.js
@@ -1,96 +1,97 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
 
-	var viewBookmarks= {
+    var viewBookmarks = {
 
 
-		init: function(){
-			this.render();
-		},
+        init: function() {
+            this.render();
+        },
 
-		getRecent: function(){
-
-
-			var deleteBookmark = function(link) {
-		        var id;
-		        id = link.attr('id');
-		        chrome.bookmarks.remove(id, function() {
-		         	link.parentsUntil("ul").slideUp(function() {
-		              $(this).remove();
-		              console.log(link);
-		            });
-		        });
-		    };
-		
-
-			var getBookmarks = function(){
-
-				$('#bookmark-link').on('click', function(e) {
-				            e.preventDefault();
-
-				              chrome.tabs.create({
-				                url: "chrome://bookmarks/"
-				              });
+        getRecent: function() {
 
 
-			    });
-				chrome.bookmarks.getRecent( 15, function(tree){
-
-					this.children = tree;
-					
-					this.children.forEach(function(recent){
-
-						console.log(recent);
-						var recentList = $("#recentlist");
-						var u = recent.url;
-						var title = recent.title.toString().slice(0,35);
-						var date = new Date(recent.dateAdded);
-						var dateTrim = date.toString().slice(0,15);
-						var favicon = 'https://www.google.com/s2/favicons?domain=' + u;
-						var favElement = '<img src="' + favicon + '"' + '>'
-						var li = $('<li></li>');
-						var $link = $('<a href="#" target="_blank" class="link"></a>');
-						var span = $('<span class="linkBlock"></span')
-						var $close = $('<i class="fa fa-times"></i>')
-						var p = $('<p class="date"></p>')
-						$link.innerHTML = recent.title;
-						$link.attr('id', recent.id);
-	          			$link.attr('href', recent.url);
-	          			$link.append(favElement);
-	          			$link.append(title);
-						date.innerHTML = dateTrim;
-						recentList.append(li);
-						li.append(span);
-						li.append(p);
-						p.append(dateTrim);
-						span.append($link);
-						span.append($close);
+            var deleteBookmark = function(link) {
+                var id;
+                id = link.attr('id');
+                chrome.bookmarks.remove(id, function() {
+                    link.parentsUntil("ul").slideUp(function() {
+                        $(this).remove();
+                        console.log(link);
+                    });
+                });
+            };
 
 
-						$close.on('click', function(e) {
-							
-				         	e.preventDefault();
-				            e.stopImmediatePropagation();
-				             deleteBookmark($link);
-				        });
+            var getBookmarks = function() {
 
-					});
-				
-				});
+                $('#bookmark-link').on('click', function(e) {
+                    e.preventDefault();
 
-			}; /*end of getBookmarks */
+                    chrome.tabs.create({
+                        url: "chrome://bookmarks/"
+                    });
 
-			getBookmarks();
 
-		}, /*end of getRecent function*/
-	
+                });
+                chrome.bookmarks.getRecent(15, function(tree) {
 
-		render: function(){
-			this.getRecent();
-		}
+                    this.children = tree;
 
-	};/*end of viewBookmarks*/
+                    this.children.forEach(function(recent) {
 
-	viewBookmarks.init();
-	 
+                        console.log(recent);
+                        var recentList = $("#recentlist");
+                        var u = recent.url;
+                        var title = recent.title.toString().slice(0, 35);
+                        var date = new Date(recent.dateAdded);
+                        var dateTrim = date.toString().slice(0, 15);
+                        var favicon = 'https://www.google.com/s2/favicons?domain=' + u;
+                        var favElement = '<img src="' + favicon + '"' + '>'
+                        var li = $('<li></li>');
+                        var $link = $('<a href="#" target="_blank" class="link"></a>');
+                        var span = $('<span class="linkBlock"></span')
+                        var $close = $('<i class="fa fa-times"></i>')
+                        var p = $('<p class="date"></p>')
+                        $link.innerHTML = recent.title;
+                        $link.attr('id', recent.id);
+                        $link.attr('href', recent.url);
+                        $link.append(favElement);
+                        $link.append(title);
+                        date.innerHTML = dateTrim;
+                        recentList.append(li);
+                        li.append(span);
+                        li.append(p);
+                        p.append(dateTrim);
+                        span.append($link);
+                        span.append($close);
+
+
+                        $close.on('click', function(e) {
+
+                            e.preventDefault();
+                            e.stopImmediatePropagation();
+                            deleteBookmark($link);
+                        });
+
+                    });
+
+                });
+
+            }; /*end of getBookmarks */
+
+            getBookmarks();
+
+        },
+        /*end of getRecent function*/
+
+
+        render: function() {
+            this.getRecent();
+        }
+
+    }; /*end of viewBookmarks*/
+
+    viewBookmarks.init();
+
 });
