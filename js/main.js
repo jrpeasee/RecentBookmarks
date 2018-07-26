fab73d10ee688e00c3555c88622766ca2b83195c
James Pease <jamespease@Jamess-MacBook-Pro-2.local>
2018-07-25 21:16:07 -0700

    added deleteAll feature

--- js/main.js
+++ js/main.js
@@ -1,58 +1,81 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-    var viewBookmarks = {
+	(function viewBookmarks(){
 
 
-        init: function() {
-            this.render();
-        },
+		(function init(){
+			generateRecent();
+		})();
 
-        getRecent: function() {
-
-
-            var deleteBookmark = function(link) {
-                var id;
-                id = link.attr('id');
-                chrome.bookmarks.remove(id, function() {
-                    link.parentsUntil("ul").slideUp(function() {
-                        $(this).remove();
-                        console.log(link);
-                    });
-                });
-            };
+		function generateRecent(){
 
 
-            var getBookmarks = function() {
+			let deleteBookmark = function(link,id) {
+		        //console.log(link);
+            
+		        chrome.bookmarks.remove(id, function() {
+		         	link.parentsUntil("ul").slideUp(function() {
+		              $(this).remove();
+		             
+		            });
+		        });
+		    };
+          
+		    let deleteBookmarks = function(){
+                let listItem = $("#recentlist li");
+                let links = $("#recentlist li a");
+                let id = links.attr('id');
+                
+                for(let i = 0; i<=listItem.length-1; i++){
+                    if(listItem.length == 0){
+                        return
+                    }else{
+                        ids = links[i].id;
+                        console.log(id);
+                        chrome.bookmarks.remove(ids,function(){
+                            links.parentsUntil("ul").slideUp(function() {
+                                $(this).remove();
+                            });
+                        })
+                    }
+                }
+                getBookmarks();
+            }
+
+
+           let bookmarkManagerLink = (function(){
 
                 $('#bookmark-link').on('click', function(e) {
-                    e.preventDefault();
+                            e.preventDefault();
 
-                    chrome.tabs.create({
-                        url: "chrome://bookmarks/"
-                    });
+                              chrome.tabs.create({
+                                url: "chrome://bookmarks/"
+                              });
 
 
                 });
-                chrome.bookmarks.getRecent(15, function(tree) {
+            })();
 
-                    this.children = tree;
+            
 
-                    this.children.forEach(function(recent) {
+		    function getBookmarks(){
+				chrome.bookmarks.getRecent( 15, function(tree){
 
-                        console.log(recent);
-                        var recentList = $("#recentlist");
-                        var u = recent.url;
-                        var title = recent.title.toString().slice(0, 35);
-                        var date = new Date(recent.dateAdded);
-                        var dateTrim = date.toString().slice(0, 15);
-                        var favicon = 'https://www.google.com/s2/favicons?domain=' + u;
-                        var favElement = '<img src="' + favicon + '"' + '>'
-                        var li = $('<li></li>');
-                        var $link = $('<a href="#" target="_blank" class="link"></a>');
-                        var span = $('<span class="linkBlock"></span')
-                        var $close = $('<i class="fa fa-times"></i>')
-                        var p = $('<p class="date"></p>')
+                    tree.forEach(function(recent){
+                        
+                        let recentList = $("#recentlist");
+                        let u = recent.url;
+                        let title = recent.title.toString().slice(0, 35);
+                        let date = new Date(recent.dateAdded);
+                        let dateTrim = date.toString().slice(0, 15);
+                        let favicon = 'https://www.google.com/s2/favicons?domain=' + u;
+                        let favElement = '<img src="' + favicon + '"' + '>'
+                        let li = $('<li></li>');
+                        let $link = $('<a href="#" target="_blank" class="link"></a>');
+                        let span = $('<span class="linkBlock"></span')
+                        let $close = $('<i class="fa fa-times"></i>')
+                        let p = $('<p class="date"></p>')
                         $link.innerHTML = recent.title;
                         $link.attr('id', recent.id);
                         $link.attr('href', recent.url);
@@ -65,33 +88,47 @@ $(document).ready(function() {
                         p.append(dateTrim);
                         span.append($link);
                         span.append($close);
-
+                        id = recent.id;
 
                         $close.on('click', function(e) {
 
                             e.preventDefault();
                             e.stopImmediatePropagation();
-                            deleteBookmark($link);
+                            deleteBookmark($link,id);
                         });
+                       /* const markup = `<li>
+                                  <span class="linkBlock">
+                                  <a id="${recent.id}" href="${recent.url}" target="_blank" class="link">${recent.title.toString().slice(0,35)}</a>
+                                  <i class="fa fa-times"></i>
+                                  </span>
+                                  <p class="date"></p>
+                                </li>`;*/
+                       
+					});
+				   
+				});/*end of getRecent */
+			}; /*end of getBookmarks */
 
-                    });
+			getBookmarks();
+           
 
-                });
+            let deleteAllbutton = document.querySelector("#deleteAllBtn");
 
-            }; /*end of getBookmarks */
+            deleteAllbutton.addEventListener('click', function(e){
+                e.preventDefault();
+                let listItem = document.getElementsByClassName('link');
+                if(listItem.length === 0){
+                   return;
+                }else if (confirm('Are you sure you want to delete all bookmarks?')) {
+                    deleteBookmarks();                  
+                }else {
+                    return;
+                }
+                
+            });
+		};/*end of generateRecent function*/
+	   
 
-            getBookmarks();
-
-        },
-        /*end of getRecent function*/
-
-
-        render: function() {
-            this.getRecent();
-        }
-
-    }; /*end of viewBookmarks*/
-
-    viewBookmarks.init();
-
+	})();/*end of viewBookmarks*/
+	 
 });
